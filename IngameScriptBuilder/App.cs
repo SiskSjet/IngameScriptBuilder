be0ca6b30377393dda8c2f89f0886f9addd9143a
Sisk S'jet <sisk.sjet@gmail.com>
2017-04-02 06:11:40 +0200

    feat: add a commandline interface
    Closes #1

---
+++ IngameScriptBuilder/App.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace IngameScriptBuilder {
+    public class App : CommandLineApplication {
+        private readonly CommandOption _excludeDirectories;
+        private readonly CommandOption _excludeFiles;
+        private readonly CommandOption _help;
+        private readonly CommandOption _minify;
+        private readonly CommandArgument _output;
+        private readonly CommandArgument _project;
+        private readonly CommandOption _removeComments;
+        private readonly CommandOption _removeDocumentations;
+        private readonly CommandOption _version;
+
+        public App() : base(false) {
+            Name = AppInfo.FileName;
+            FullName = AppInfo.Title;
+            Description = AppInfo.Description;
+
+            _project = Argument("Project", "Directory which include all your source files or a .csproj project file");
+            _output = Argument("Output", "If empty script is generated in clipboard else to specified file");
+
+            _minify = Option("-m | --minify", "Minify the ingame script", CommandOptionType.NoValue);
+            _removeComments = Option("-r | --removeComments", "Generate script without comments", CommandOptionType.NoValue);
+            _removeDocumentations = Option("-rd | --removeDocumentation", "Generate script without documentations", CommandOptionType.NoValue);
+            _excludeDirectories = Option("-xd | --excludeDirectory", "Exclude an directory", CommandOptionType.MultipleValue);
+            _excludeFiles = Option("-xf | --excludefile", "Exclude a file", CommandOptionType.MultipleValue);
+
+            _version = Option("-v | --version", "Show version information", CommandOptionType.NoValue);
+            _help = Option("-h | --help", "Show help information", CommandOptionType.NoValue);
+
+            OnExecute((Func<int>) RunCommand);
+        }
+
+        public new void ShowHelp(string commandName = null) {
+            var lines = Regex.Split(GetHelpText(commandName), "\r\n|\r|\n");
+            lines[0] = $"{FullName} [Version {AppInfo.AssemblyVersion}]{Environment.NewLine}{Description}";
+            Console.WriteLine(string.Join(Environment.NewLine, lines));
+        }
+
+        public new void ShowVersion() {
+            Console.WriteLine(AppInfo.ProductVersion);
+        }
+
+        private int RunCommand() {
+            if (_help.HasValue()) {
+                ShowHelp();
+                return 0;
+            }
+            if (_version.HasValue()) {
+                ShowVersion();
+                return 0;
+            }
+
+            if (string.IsNullOrWhiteSpace(_project.Value)) {
+                Console.WriteLine("A project file or directory is required.");
+                return 1;
+            }
+
+            if (!(Path.HasExtension(_project.Value) && Path.GetExtension(_project.Value) == ".csproj"))
+                if (!Directory.Exists(_project.Value)) {
+                    Console.WriteLine("No valid project found at {0}", _project.Value);
+                    return 1;
+                }
+
+            var project = _project.Value;
+            var output = _output.Value;
+            var minify = _minify.HasValue();
+            var removeComments = _removeComments.HasValue();
+            var removeDocumentations = _removeDocumentations.HasValue();
+            var excludeFiles = _excludeFiles.Values;
+            var excludeDirectories = _excludeDirectories.Values;
+
+            // todo: implement a generate method.
+            // Generator.Generate(project, minify, removeComments, removeDocumentations, excludeFiles, excludeDirectories)
+
+            return 0;
+        }
+    }
+}
--- IngameScriptBuilder/Program.cs
+++ IngameScriptBuilder/Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace IngameScriptBuilder {
     class Program {
-        static void Main(string[] args) {
-
+        static int Main(string[] args) {
+            try {
+                return new App().Execute(args);
+            } catch (Exception ex) {
+                Console.Write(ex);
+                return 1;
+            }
         }
     }
 }
