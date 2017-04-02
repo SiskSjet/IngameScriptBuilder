c4523b5ebba56e820d34966831c12eaada34fe93
Sisk S'jet <sisk.sjet@gmail.com>
2017-04-02 05:46:19 +0200

    style(AppInfo): sort members in AppInfo.cs

--- IngameScriptBuilder/AppInfo.cs
+++ IngameScriptBuilder/AppInfo.cs
@@ -5,21 +5,22 @@ using System.Reflection;
 namespace IngameScriptBuilder {
     public static class AppInfo {
         private static readonly Assembly Assembly = Assembly.GetEntryAssembly();
-        private static string _title;
-        private static string _description;
+        private static Version _assemblyVersion;
         private static string _company;
         private static string _copyright;
-        private static Version _assemblyVersion;
+        private static string _description;
+        private static string _fileName;
         private static Version _fileVersion;
         private static string _product;
         private static string _productVersion;
-        private static string _fileName;
+        private static string _title;
 
-        public static string Title => _title ?? (_title = GetCustomAttributes<AssemblyTitleAttribute>().Title);
-        public static string Description => _description ?? (_description = GetCustomAttributes<AssemblyDescriptionAttribute>().Description);
+        public static Version AssemblyVersion => _assemblyVersion ?? (_assemblyVersion = Assembly.GetName().Version);
         public static string Company => _company ?? (_company = GetCustomAttributes<AssemblyCompanyAttribute>().Company);
         public static string Copyright => _copyright ?? (_copyright = GetCustomAttributes<AssemblyCopyrightAttribute>().Copyright);
-        public static Version AssemblyVersion => _assemblyVersion ?? (_assemblyVersion = Assembly.GetName().Version);
+        public static string Description => _description ?? (_description = GetCustomAttributes<AssemblyDescriptionAttribute>().Description);
+        public static string FileName => _fileName ?? (_fileName = Path.GetFileName(Location));
+
         public static Version FileVersion {
             get {
                 if (_fileVersion == null) {
@@ -30,16 +31,18 @@ namespace IngameScriptBuilder {
                 return _fileVersion;
             }
         }
+
+        public static string Location => Assembly.Location;
         public static string Product => _product ?? (_product = GetCustomAttributes<AssemblyProductAttribute>().Product);
         public static string ProductVersion => _productVersion ?? (_productVersion = GetCustomAttributes<AssemblyInformationalVersionAttribute>().InformationalVersion);
-        public static string FileName => _fileName ?? (_fileName = Path.GetFileName(Location));
-        public static string Location => Assembly.Location;
+
+        public static string Title => _title ?? (_title = GetCustomAttributes<AssemblyTitleAttribute>().Title);
 
         private static T GetCustomAttributes<T>() where T : Attribute {
             var attributes = Assembly.GetCustomAttributes(typeof(T), true);
             if (attributes.Length == 0)
                 return null;
-            return (T)attributes[0];
+            return (T) attributes[0];
         }
     }
 }
