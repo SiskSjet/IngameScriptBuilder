7d946d994a877133765aa023116f5f2cfe2f34b8
Sisk S'jet <sisk.sjet@gmail.com>
2017-04-15 13:04:55 +0200

    refactor: move SyntaxRewriters to own namespace

---
+++ IngameScriptBuilder/SyntaxRewriter/NoDocumentationSyntaxRewriter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace IngameScriptBuilder.SyntaxRewriter {
+    public class NoDocumentationSyntaxRewriter : CSharpSyntaxRewriter {
+        public override SyntaxTrivia VisitTrivia(SyntaxTrivia trivia) {
+            if (trivia.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia)
+                || trivia.IsKind(SyntaxKind.MultiLineDocumentationCommentTrivia)
+                || trivia.IsKind(SyntaxKind.EndOfDocumentationCommentToken)
+                || trivia.IsKind(SyntaxKind.DocumentationCommentExteriorTrivia)) {
+                return default(SyntaxTrivia);
+            }
+            return base.VisitTrivia(trivia);
+        }
+    }
+}
